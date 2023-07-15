89d91ccf05cad711d275fb38a73bc6131ef1eee9
Sakana <pr0zel@gmail.com>
2023-07-15 22:05:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i 's/CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND/CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL/g' target/linux/x86/64/config-5.15
 #sed -i '/CONFIG_CPU_FREQ_GOV_ONDEMAND/a\CONFIG_CPU_FREQ_GOV_SCHEDUTIL=y' target/linux/x86/64/config-5.15
+
+sed -i 's/CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND=y/CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL=y/g' target/linux/x86/64/config-5.15
+sed -i 's/# CONFIG_CPU_FREQ_GOV_CONSERVATIVE is not set/CONFIG_CPU_FREQ_GOV_CONSERVATIVE=y/g' target/linux/x86/64/config-5.15
