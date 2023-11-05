0a922f81d0c41bbc3b88d5705b3c9173d0696fd9
Sakana <pr0zel@gmail.com>
2023-11-05 17:38:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git ./package/luci-app-tcpdump
+git clone https://github.com/gSpotx2f/luci-app-disks-info.git ./package/luci-app-disks-info
 git clone https://github.com/gw826943555/openwrt-cpufreq.git ./package/luci-app-cpufreq
 
